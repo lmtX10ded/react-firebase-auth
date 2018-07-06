e495f24e8216b36b29907e98d2bc0d9f54a58ef1
lmtX10ded <kishan.psd@gmail.com>
2018-07-06 16:54:47 -0400

    i

---
+++ src/firebase/firebase.js
@@ -0,0 +1,33 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+  // Initialize Firebase
+  const devConfig = {
+    apiKey: "AIzaSyAlnM3c8oM8nuGJ79NN229D6NnqZy3P7Kg",
+    authDomain: "react-firebase-auth-9f55f.firebaseapp.com",
+    databaseURL: "https://react-firebase-auth-9f55f.firebaseio.com",
+    projectId: "react-firebase-auth-9f55f",
+    storageBucket: "react-firebase-auth-9f55f.appspot.com",
+    messagingSenderId: "365818039922"
+  };
+
+  // Initialize Firebase
+  const proConfig = {
+    apiKey: "AIzaSyCivFKk8HbDoRnloAPZz0czASraezYEDmE",
+    authDomain: "react-firebase-auth-pro.firebaseapp.com",
+    databaseURL: "https://react-firebase-auth-pro.firebaseio.com",
+    projectId: "react-firebase-auth-pro",
+    storageBucket: "react-firebase-auth-pro.appspot.com",
+    messagingSenderId: "920240871595"
+  };
+
+  const config = process.env.NODE_ENV === 'production'
+    ? proConfig
+    : devConfig;
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export { auth, };
